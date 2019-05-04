ab0b104932261d1689239f7c560a316b29796e5c
Jack Grigg <jack@z.cash>
2019-05-04 17:47:18 +0100

    Move redjubjub into zcash_primitives

---
+++ src/util.rs
@@ -0,0 +1,10 @@
+use blake2b_simd::Params;
+use sapling_crypto::jubjub::{JubjubEngine, ToUniform};
+
+pub fn hash_to_scalar<E: JubjubEngine>(persona: &[u8], a: &[u8], b: &[u8]) -> E::Fs {
+    let mut hasher = Params::new().hash_length(64).personal(persona).to_state();
+    hasher.update(a);
+    hasher.update(b);
+    let ret = hasher.finalize();
+    E::Fs::to_uniform(ret.as_ref())
+}

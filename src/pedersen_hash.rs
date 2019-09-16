QED-it/sapling-crypto-internal

pedersen hashes: example of size limit bug

src/pedersen_hash.rs
@@ -19,7 +19,7 @@ impl Personalization {
                 (0..6).map(|i| (num >> i) & 1 == 1).collect()
             }
             Personalization::Empty => {
-                vec![true, true, true, false, false, false]
+                vec![true, true, true, true, true, true]
             }
         }
     }
@@ -112,12 +112,23 @@ pub fn pedersen_hash<E, I>(
 #[cfg(test)]
 mod test {
     use ::jubjub::*;
+    use pairing::bls12_381::{Bls12, Fr};
+    use pedersen_hash::{pedersen_hash, Personalization};
 
     #[test]
-    fn test_pedersen_hash_generators() {
-        let params = &JubjubBls12::new();
+    fn test_pedersen_hash_noncircuit() {
+        let params  = &JubjubBls12::new();
+        /*
         for (i, generator) in params.pedersen_hash_generators().iter().enumerate() {
             println!("generator {}, x={}, y={}", i, generator.into_xy().0, generator.into_xy().1)
         }
+        */
+
+        let mut input: Vec<bool> = vec![];
+        for i in 0..(63*3*4+1) {
+            input.push(true);
+        }
+        let p = pedersen_hash::<Bls12, _>(Personalization::Empty, input, &params).into_xy();
+        println!("hash = {}, {}", p.0, p.1);
     }
 }

zcashme/zns-zcash_primitives

src/pedersen_hash.rs
@@ -18,7 +18,7 @@ impl Personalization {
                 (0..6).map(|i| (num >> i) & 1 == 1).collect()
             }
             Personalization::Empty => {
-                vec![true, true, true, false, false, false]
+                vec![true, true, true, true, true, true]
             }
         }
     }
@@ -115,13 +115,26 @@ where
 
 #[cfg(test)]
 mod test {
-    use crate::jubjub::*;
+    use crate::{
+        jubjub::*,
+        pedersen_hash::{pedersen_hash, Personalization},
+    };
+    use pairing::bls12_381::{Bls12, Fr};
 
     #[test]
-    fn test_pedersen_hash_generators() {
+    fn test_pedersen_hash_noncircuit() {
         let params = &JubjubBls12::new();
+        /*
         for (i, generator) in params.pedersen_hash_generators().iter().enumerate() {
             println!("generator {}, x={}, y={}", i, generator.to_xy().0, generator.to_xy().1)
         }
+        */
+
+        let mut input: Vec<bool> = vec![];
+        for i in 0..(63*3*4+1) {
+            input.push(true);
+        }
+        let p = pedersen_hash::<Bls12, _>(Personalization::Empty, input, &params).to_xy();
+        println!("hash = {}, {}", p.0, p.1);
     }
 }
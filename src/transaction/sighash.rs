ShieldedLabs/librustzcash

Renamed of the zip233_amount field, removed the zcash_unstable = "nsm" flag, added NU7

zcash_primitives/src/transaction/sighash.rs
@@ -39,7 +39,7 @@ impl SignableInput<'_> {
         match self {
             SignableInput::Shielded => ::transparent::sighash::SIGHASH_ALL,
             SignableInput::Transparent(input) => input.hash_type().encode(),
-            #[cfg(zcash_unstable = "zfuture")]
+            #[cfg(zcash_unstable = "tze")]
             SignableInput::Tze { .. } => ::transparent::sighash::SIGHASH_ALL,
         }
     }
@@ -72,6 +72,8 @@ pub fn signature_hash<
         }
 
         TxVersion::Zip225 => v5_signature_hash(tx, signable_input, txid_parts),
+
+        TxVersion::Zip230 => v5_signature_hash(tx, signable_input, txid_parts),
 
         #[cfg(zcash_unstable = "zfuture")]
         TxVersion::ZFuture => v5_signature_hash(tx, signable_input, txid_parts),

zcashme/zns-zcash_primitives

src/transaction/sighash.rs
@@ -74,7 +74,7 @@ pub fn signature_hash<
         TxVersion::V5 => v5_signature_hash(tx, signable_input, txid_parts),
 
         #[cfg(zcash_unstable = "nu7")]
-        TxVersion::V6 => todo!(),
+        TxVersion::V6 => v5_signature_hash(tx, signable_input, txid_parts),
         #[cfg(zcash_unstable = "zfuture")]
         TxVersion::ZFuture => v5_signature_hash(tx, signable_input, txid_parts),
     })